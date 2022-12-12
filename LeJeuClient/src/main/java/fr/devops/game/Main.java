c299bfee6b86b3dc0ba2f1193abc01d816d4dc41
XxGoldenbluexX <tonyg222@outlook.fr>
2022-12-12 10:52:07 +0100

    Je tente des trucs

---
+++ LeJeuClient/src/main/java/fr/devops/game/Main.java
@@ -0,0 +1,8 @@
+package fr.devops.game;
+
+public class Main {
+
+	public static void main(String[] args) {
+    }
+	
+}
--- LeJeuClient/src/main/java/module-info.java
+++ LeJeuClient/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module fr.devops.game {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.graphics;
 
 
     opens fr.devops.game to javafx.fxml;
